177f7b601662f9c169790bff27de15142a179da1
Margaret Leibovic <margaret.leibovic@gmail.com>
2013-09-17 14:17:56 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,68 @@
+var bz = require("bz"),
+    irc = require("irc");
+
+if (module.parent) {
+  return;
+}
+
+var bot = new irc.Client("irc.mozilla.org", "fennecbot", {
+  channels: ["#mobile"],
+  secure: true,
+  port: 6697
+});
+
+var bugzilla = bz.createClient();
+
+// Finds a bug that matches the search term, and says it to the person who asked about it.
+function findBug(from, to, search) {
+  bugzilla.searchBugs({ quicksearch: search }, function(error, bugs) {
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    // Find a random bug from the array.
+    var index = Math.floor(Math.random() * bugs.length);
+    var bug = bugs[index];
+    console.log(bot.nick + " found bug " + bug.id);
+
+    var message = from + ": Try working on bug " + bug.id + " - " + bug.summary + " http://bugzil.la/" + bug.id;
+    bot.say(to, message);
+  });
+}
+
+bot.addListener("message", function(from, to, message) {
+  if (message.indexOf(bot.nick) !== 0) {
+    return;
+  }
+  
+  if (message.indexOf("tracking bug") > -1) {
+    findBug(from, to, "blocking-fennec:+ @nobody");
+    return;
+  }
+
+  if (message.indexOf("mentor bug") > -1) {
+    findBug(from, to, "prod:android sw:mentor @nobody");
+    return;
+  }
+
+  if (message.indexOf("help") > -1) {
+    bot.say(to, from + ": Try looking at our Get Involved page https://wiki.mozilla.org/Mobile/Get_Involved");
+    return;
+  }
+
+  if (message.indexOf("triage") > -1) {
+    bot.say(to, "https://wiki.mozilla.org/Mobile/Triage");
+    return;
+  }
+
+  if (message.indexOf("notes") > -1) {
+    bot.say(to, "https://wiki.mozilla.org/Mobile/Notes");
+    return;
+  }
+
+  if (message.indexOf("build") > -1) {
+    bot.say(to, "https://wiki.mozilla.org/Mobile/Fennec/Android");
+    return;
+  }
+});

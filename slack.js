ed8f0e451b1ca48e6d2339367ba922d6a5d1402b
Margaret Leibovic <margaret.leibovic@gmail.com>
2015-05-18 13:12:39 -0700

    actual slack bot support

--- slack.js
+++ slack.js
@@ -1,12 +1,27 @@
-var Slack = require('node-slack'),
+var Slack = require('slack-client'),
     config = require("./slack-config");
 
-var slack = new Slack(config.hook_url);
+var slack = new Slack(config.token, true, true);
 
-slack.send({
-  text: 'Another test',
-  channel: '#mobile',
-  username: 'fennecbot'
+slack.on('message', function(message) {
+  var channel = slack.getChannelGroupOrDMByID(message.channel);
+  var user = slack.getUserByID(message.user);
+
+  if (message.type !== 'message' || !message.text || !channel) {
+    return;
+  }
+
+  if (message.text.indexOf(slack.self.name) !== 0) {
+    return;
+  }
+
+  var response = user.name + ": Hi!";
+  channel.send(response);
+  return console.log("@" + slack.self.name + " responded with \"" + response + "\"");
 });
 
+slack.on('error', function(error) {
+  return console.error("Error: " + error);
+});
 
+slack.login();
